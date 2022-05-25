72f05e5742c97e840ededd6f6230aad483f61112
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-05-25 23:48:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  //onchange:-
+  const [nvalue, setNvalue] = useState("");
+
+  const saveValue = () => {
+    fetch("http://localhost:8080/todos", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+       title: nvalue,
+        author:false
+      })
+    })
+      .then((r) => r.json())
+      .then((d) => {
+        setTodos([...todos, d])
+        setNvalue("");
+      });
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/todos?_page=1&_limit=2")
+      .then((r) => r.json())
+      .then((d) => {
+         console.log(d)
+        setTodos(d)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => {
+        setNvalue(e.target.value)
+        
+      }} />
+      <button onClick={saveValue}> + </button>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.id}.{todo.title}</div>
+      ))}
     </div>
   );
 }
